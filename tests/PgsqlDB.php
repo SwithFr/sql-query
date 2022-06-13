8bfa115ccfe5ccaccfc78ee6c1c23d4dc9b14952
SwithFr <jeremy@docmarketing.fr>
2022-06-13 10:10:46 +0200

    test with env variables

--- tests/PgsqlDB.php
+++ tests/PgsqlDB.php
@@ -13,6 +13,7 @@ class PgsqlDB implements DBInterface
     public function __construct()
     {
         try {
+            dump($_ENV);
             $this->pdo = new PDO("pgsql:host=localhost;port=5432;dbname=root;user=root;password=root");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
