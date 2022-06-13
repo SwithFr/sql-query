0dd6b94fdc47c657dbe3d579c1cd1b5440ae3604
SwithFr <jeremy@docmarketing.fr>
2022-06-13 10:26:29 +0200

    psql host

--- tests/PgsqlDB.php
+++ tests/PgsqlDB.php
@@ -13,7 +13,7 @@ class PgsqlDB implements DBInterface
     public function __construct()
     {
         try {
-            $host = getenv('POSTGRES_HOST') ?: 'localhost';
+            $host = 'localhost';
             $this->pdo = new PDO("pgsql:host=$host;port=5432;dbname=root;user=root;password=root");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
