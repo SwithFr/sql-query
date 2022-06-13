a17ec87c93459d762e81e2277d50264bd7a57a66
SwithFr <jeremy@docmarketing.fr>
2022-06-13 10:27:42 +0200

    remove useless host variable

--- tests/PgsqlDB.php
+++ tests/PgsqlDB.php
@@ -13,8 +13,7 @@ class PgsqlDB implements DBInterface
     public function __construct()
     {
         try {
-            $host = 'localhost';
-            $this->pdo = new PDO("pgsql:host=$host;port=5432;dbname=root;user=root;password=root");
+            $this->pdo = new PDO("pgsql:host=localhost;port=5432;dbname=root;user=root;password=root");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         } catch (PDOException $e) {
